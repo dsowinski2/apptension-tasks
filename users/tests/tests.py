c61bda67499a1f51712b83a8e6dfcd9a98835118
Damian Sowiński <dsowinski@apptension.com>
2021-09-10 10:27:00 +0200

    Remove unnecessary method in creating users factories

--- users/tests/tests.py
+++ users/tests/tests.py
@@ -14,13 +14,13 @@ def get_tokens_for_user(user):
 
 class TestUserObtainToken:
 	def test_fail_to_obtain_user_token(self, client, user_factory):
-		user = user_factory.create()
+		user = user_factory()
 		response = client.post(reverse('token_obtain_pair'), {"email": user.email, "password": "wrong_password"}, format='json')
 		
 		assert response.status_code == 401
 
 	def test_obtain_user_token(self, client, user_factory):
-		user = user_factory.create()
+		user = user_factory()
 		response = client.post(reverse('token_obtain_pair'), {"email": user.email, "password": user.plain_password}, format='json')
 		
 		assert response.status_code == 200, response.data
@@ -32,20 +32,20 @@ class TestListUsers:
 		assert response.status_code == 401
 
 	def test_list_users_authorization(self, client, user_factory):
-		user = user_factory.create()
+		user = user_factory()
 		response = client.get(reverse('list_users'), HTTP_AUTHORIZATION=get_tokens_for_user(user))
-
+		
 		assert response.status_code == 200, response.data
 
 	def test_list_users_company_details(self, client, company_user_factory):
-		user = company_user_factory.create()
+		user = company_user_factory()
 		response = client.get(reverse('list_users'), HTTP_AUTHORIZATION=get_tokens_for_user(user))
 		
 		assert response.status_code == 200
 		assert response.data[0]['details']['vat_id']
 
 	def test_list_users_user_details(self, client, user_factory):
-		user = user_factory.create()
+		user = user_factory()
 		response = client.get(reverse('list_users'), HTTP_AUTHORIZATION=get_tokens_for_user(user))
 		
 		assert response.status_code == 200
@@ -53,15 +53,15 @@ class TestListUsers:
 		assert response.data[0]['details']['street']
 
 	def test_list_users_no_admins(self, client, admin_user_factory):
-		user = admin_user_factory.create()
+		user = admin_user_factory()
 		response = client.get(reverse('list_users'), HTTP_AUTHORIZATION=get_tokens_for_user(user))
 
 		assert response.status_code == 200
 		assert len(response.data) == 0
 
 	def test_both_type_of_users_returned(self, client, user_factory, company_user_factory):
-		user = user_factory.create()
-		user_2 = company_user_factory.create()
+		user = user_factory()
+		user_2 = company_user_factory()
 		response = client.get(reverse('list_users'), HTTP_AUTHORIZATION=get_tokens_for_user(user))
 
 		assert response.status_code == 200
